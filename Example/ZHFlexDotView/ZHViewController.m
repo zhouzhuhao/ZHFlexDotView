9ad1e42b6c9220a4ecee472f48f93657937ed3fb
Zhuhao Chow <johnzhuhaochow@gmail.com>
2019-07-12 11:21:37 +0800

    修改demo,flex view 需要添加到window上

--- Example/ZHFlexDotView/ZHViewController.m
+++ Example/ZHFlexDotView/ZHViewController.m
@@ -7,10 +7,10 @@
 //
 
 #import "ZHViewController.h"
-
+#import <ZHFlexDotView/ZHFlexDotView-umbrella.h>
 
 @interface ZHViewController ()
-
+@property (nonatomic,strong) ZHFlexDotView *dotView;
 @end
 
 @implementation ZHViewController
@@ -19,13 +19,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-	
+
 
 }
 
 - (void)viewDidAppear:(BOOL)animated{
 	[super viewDidAppear:animated];
-
+#pragma mark -DEBUG模式下才显示Flex,如果需要打包后的ipa也出现Flex,就需要在scheme中配置Archive时为debug ,需要window出现才能添加
+#if DEBUG
+	NSEnumerator *frontToBackWindows = [[[UIApplication sharedApplication] windows] reverseObjectEnumerator];
+	for (UIWindow *win in frontToBackWindows) {
+		if (win.windowLevel == UIWindowLevelNormal) {
+			self.dotView = [[ZHFlexDotView alloc] initWithFrame:CGRectMake(0, 60, 80, 80)];
+			[win addSubview:self.dotView];
+			break;
+		}
+	}
+#endif
 }
 
 - (void)didReceiveMemoryWarning
--- ZHFlexDotView.podspec
+++ ZHFlexDotView.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ZHFlexDotView'
-  s.version          = '1.0.0'
+  s.version          = '1.0.3'
   s.summary          = 'Quick show flex tool'
 
 # This description is used to generate tags and improve search results.
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+        Flex 小浮窗
                        DESC
 
   s.homepage         = 'https://github.com/zhouzhuhao/ZHFlexDotView'
